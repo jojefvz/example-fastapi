72dc54a6d59f49da57abe13fc61d3dfda4450d2b
Josiah <112897472+jojefvz@users.noreply.github.com>
2024-10-17 09:46:09 -0500

    validation added to config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings
-
+from pydantic import ValidationError
 
 class Settings(BaseSettings):
     database_hostname: str
@@ -14,4 +14,12 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
 
-settings = Settings()
+try:
+    settings = Settings()
+except ValidationError as e:
+    # Print the entire error details including input_value
+    print(e.errors())  # this will show the full error details
+    # If you specifically want to print the input_value
+    for error in e.errors():
+        if "input_value" in error:
+            print("Input Value: ", error["input_value"])
